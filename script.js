4b986225d4cfa45a2f2eb8fdb149813d104ac151
Ryan Dulac <analyticrye@gmail.com>
2019-08-13 22:47:12 -0400

    need a working version to rollback to

---
+++ script.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+const register = require("./controllers/register");
+const signIn = require("./controllers/signIn");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "yofuck56",
+    database: "braindb"
+  }
+});
+
+db.select("*")
+  .from("users")
+  .then(data => {
+    console.log(data);
+  });
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+app.post("/signIn", signIn.handleSignIn(db, bcrypt));
+
+app.post("/register", register.handleRegister(db, bcrypt));
+
+app.get("/profile/:id", profile.handleProfile(db));
+
+app.put("/image", image.handleImage(db));
+
+app.listen(3000, () => {
+  console.log("App is running on port 3000");
+});
